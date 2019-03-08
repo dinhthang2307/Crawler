10b7f906a724d659968e80dec181a5324857f16e
FSOFT.FPT.VN\ThangVD10 <ThagnVD10@fsoft.com.vn>
2019-03-08 12:53:10 +0700

    init

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startCrawlerAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task startCrawlerAsync()
+        {
+            var url = "https://tedu.com.vn/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+
+            htmlDocument.LoadHtml(html);
+            Console.Write(html);
+        }
+    }
+}
